b0a5e3893794016926a5cb1461cfe32485fc964a
unknown <claudio.g.freitas@outlook.com>
2023-05-09 22:52:04 -0300

    criado seed no banco de dados, rotas simples de count users, pools and guesses  -  aula 2 | -1.05.16-17

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client';
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
 	log: ['query'],
-});
+})
 
 async function bootstrap() {
 	const fastify = Fastify( {
@@ -14,13 +16,47 @@ async function bootstrap() {
 	await fastify.register(cors, {
 		origin: true,
 	})
+
 	fastify.get('/pools/count', async () => {
 		const count = await prisma.pool.count();
 		
 		return { count };
 	})
 
+	fastify.get('/users/count', async () => {
+		const count = await prisma.user.count();
+		
+		return { count };
+	})
+
+	fastify.get('/guesses/count', async () => {
+		const count = await prisma.guess.count();
+		
+		return { count };
+	})
+
+	fastify.post('/pools', async (request, reply) => {
+		const creatPoolBody = z.object({
+			title: z.string(),
+		})
+
+		const { title } = creatPoolBody.parse(request.body)
+
+		const generate = new ShortUniqueId({ length: 6 })
+		const code = String(generate()).toUpperCase();
+
+		await prisma.pool.create({
+			data: {
+				title,
+				code
+			}
+		})
+
+		return reply.status(201).send({ code })
+		//return { title }
+	})
+
 	await fastify.listen({ port: 3333 /*, host: '0.0.0.0'*/ });
-};
+}
 
 bootstrap();
