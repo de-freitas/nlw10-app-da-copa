5efa88ce8c3df19c02e9c5ace6b4b8f54437de1e
unknown <claudio.g.freitas@outlook.com>
2023-05-13 13:23:22 -0300

    trocando o pc --

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,9 +4,15 @@ import appPreviewImg from '../assets/app-nlw-copa-preview.png'
 import logoImg from '../assets/logo.svg'  
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
+import { api } from '../lib/axios'
+
+interface HomeProps {
+  poolCount: number,
+  guessCount: number,
+}
 
 
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
@@ -45,7 +51,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt=""/>
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+2.034</span>
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
               <span>Bolões criados</span>
             </div>
           </div>
@@ -55,7 +61,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt=""/>
              <div className="flex flex-col">
-               <span className="font-bold text-2xl">+192.847</span>
+               <span className="font-bold text-2xl">+{props.guessCount}</span>
                <span>Palpites enviados</span>
             </div>
           </div> 
@@ -72,13 +78,14 @@ export default function Home() {
 //   count: number;
 // }
 
-// export const getServerSideProps = async() => {
-//   const response = await fetch('http://localhost:3333/pools/count')
-//   const data = await response.json()
+export const getServerSideProps = async() => {
+  const poolCountResponse = await api.get('pools/count')
+  const guessCountResponse = await api.get('guesses/count')
 
-//   return {
-//     props: {
-//       count: data.count,
-//     }
-//   }
-// }
+  return {
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+    }
+  }
+}
