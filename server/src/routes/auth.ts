e910e974a61cb02b8ae6181c69c4894ae3760241
Claudio Gabriel Queiroz de Freitas <claudio.freitas@ayoshii.com.br>
2023-05-30 22:49:38 -0300

    Criação de bolão com usuário logado, entrar em um bolão existente e bolões que eu participo  -  Aula 4 | -01.09.15

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -6,13 +6,10 @@ import { authenticate } from "../plugins/authenticate";
 
 export async function authRoutes(fastify: FastifyInstance) {
 
-  fastify.get(
-    '/me', 
-    {
+  fastify.get('/me', {
       onRequest: [ authenticate ],
-    },
-    async(request) => {
-    return { user: request.user }
+    }, async(request) => {
+      return { user: request.user }
   })
 
 
