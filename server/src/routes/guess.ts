8f7c12f3b79dafc38b7547d96c69d2cc7d995276
Claudio Gabriel Queiroz de Freitas <claudio.freitas@ayoshii.com.br>
2023-06-14 22:33:41 -0300

    Finalizada listagem de jogos de um bolão, criação de um palpite (com validações: se já foi enviado ou se a data ainda permite). Com isso, finalizado todo o backend da aplicação  -  aulda 4 | 01:18h

--- server/src/routes/guess.ts
+++ server/src/routes/guess.ts
@@ -41,13 +41,50 @@ export async function guessRoutes(fastify: FastifyInstance) {
 				message: "You are not allowed to create a guess inside this pool!"
 			})
 		}
-		
-		return {
-			poolId,
-			gameId,
-			firstTeamPoints,
-			secondTeamPoints,
+
+		const guess = await prisma.guess.findUnique({
+			where: {
+				participantId_gameId: {
+					participantId: participant.id,
+					gameId
+				}
+			}
+		})
+
+		if (guess) {
+			return reply.status(400).send({
+				message: "You are sent a guess to this game on this pool!"
+			})
 		}
+
+		const game = await prisma.game.findUnique({
+			where: {
+				id: gameId,
+			}
+		})
+
+		if (!game) {
+			return reply.status(400).send({
+				message: "Game not found!"
+			})
+		}
+		
+		if (game.date < new Date()) {
+			return reply.status(400).send({
+				message: "You cannot send guesses after the game date!"
+			})
+		}
+
+		await prisma.guess.create({
+			data: {
+				gameId,
+				participantId: participant.id,
+				firstTeamPoints,
+				secondTeamPoints,
+			}
+		})
+
+		return reply.status(201).send();
 
 	})
 };
