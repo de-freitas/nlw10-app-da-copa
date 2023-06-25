b8d2417377708a35333f4208fef227fe243c2038
Claudio Gabriel Queiroz de Freitas <claudio.freitas@ayoshii.com.br>
2023-06-24 22:23:06 -0300

    Listando os bolões criados, utilizando mensagem de toast, utilizada FlatList e List Empty Component

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react'
-import { VStack, Icon } from 'native-base';
+import { useEffect, useState } from 'react'
+import { VStack, Icon, useToast, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -7,18 +7,32 @@ import { api } from '../services/api'
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
-import { PoolCard } from '../components/PoolCard';
+import { Loading } from '../components/Loading';
+import { EmptyPoolList } from '../components/EmptyPoolList';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
 
 export function Pools() {
-  const { navigate } = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>();
 
+  const { navigate } = useNavigation();
+  const toast = useToast();
   async function fetchPoosl() {
     try {
+      setIsLoading(true)
       const response = await api.get('/pools')
-      console.log(response.data.pools)
+      setPools(response.data.pools)
 
     } catch (error) {
         console.log(error)
+        toast.show({
+          title: 'Não foi possível carregar os bolões',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -28,6 +42,7 @@ export function Pools() {
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
+
       <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
         <Button 
           title="BUSCAR BOLÃO POR CÓDIGO" 
@@ -35,6 +50,18 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+    
+      {
+        isLoading? <Loading /> : 
+          <FlatList 
+            data={pools}
+            keyExtractor={item => item.id}  
+            renderItem={ ({item}) => <PoolCard data={item} /> }
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={() => <EmptyPoolList/>}
+            _contentContainerStyle={{ pb: 10 }}
+            px={5}
+      />}
     </VStack>
   )
 }
